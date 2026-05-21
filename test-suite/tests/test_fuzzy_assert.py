34705cac8112905be77a6f079febfe2374005e9c
user <>
2026-05-21 16:47:13 +0200

    fix: uncommented test in fuzzy assert

--- test-suite/tests/test_fuzzy_assert.py
+++ test-suite/tests/test_fuzzy_assert.py
@@ -1,11 +1,11 @@
 import pytest
 from helper import *
 
-""" def test_simple_fuzzy_assert():
+def test_simple_fuzzy_assert():
     response = send("how do I center a div in CSS?")
-    fuzzy_assert(response, "The message should provide instructions on centering a div in CSS.") """
+    fuzzy_assert(response, "The message should provide instructions on centering a div in CSS.")
 
-def test_fuzzy_assert_with_irrelevant_response():
+def test_fuzzy_assert_expected_failure():
     response = send("how do I center a div in CSS?")
     with pytest.raises(AssertionError):
         fuzzy_assert(response, "The message should provide instructions on how to bake a cake.")
